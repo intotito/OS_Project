1a4f06cc92eb1691442a9b241dda72b031996ba8
intotito <intotito@gmail.com>
2023-01-01 20:32:28 +0000

    Comments started....

--- ie/atu/sw/os/Savable.java
+++ ie/atu/sw/os/Savable.java
@@ -1,6 +1,22 @@
 package ie.atu.sw.os;
 
+/**
+ * This interface describes the csv (Comma Separated Value) format for an implementing class.
+ * It has a single public method {@link #asString() asString}, which returns a String description
+ * of the class in csv format. 
+ * @author intot
+ *
+ */
+@FunctionalInterface
 public interface Savable{
+	/**
+	 * Escape character for replacing ',' in csv processing.
+	 * This is necessary to avoid commas ',' found within documents interfering with formatting.
+	 */
 	public static final char ESCAPE_CHAR = 'δ';
+	/**
+	 * 
+	 * @return CSV representation of the implementing class.
+	 */
 	public String asString();	
 }
--- ie/atu/sw/os/User.java
+++ ie/atu/sw/os/User.java
@@ -2,46 +2,105 @@ package ie.atu.sw.os;
 
 import java.io.Serializable;
 
-public class User implements Savable, Serializable{
+/**
+ * This class maps to a single employee on the database. It encapsulates the
+ * properties and possible actions of a given user of the system.
+ * 
+ * @author intot
+ *
+ */
+public class User implements Savable, Serializable {
+	/**
+	 * Eclipse Auto generated Number
+	 */
+	private static final long serialVersionUID = -3609332166721774386L;
+
+	/**
+	 * Convenient toString method for debugging
+	 */
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", dept=" + dept + "]";
 	}
 
-	private String id, name, email, dept;
+	/**
+	 * Id of the User
+	 */
+	private String id;
+	/**
+	 * Name of the User
+	 */
+	private String name;
+	/**
+	 * Email of the User
+	 */
+	private String email;
+	/**
+	 * Department of the User
+	 */
+	private String dept;
 
+	/**
+	 * Accessor method to get the Id
+	 * @return - The Id of the User
+	 */
 	public String getId() {
 		return id;
 	}
-
+/**
+ * Accessor method to set the Id
+ * @param id - The id to assign to the user
+ */
 	public void setId(String id) {
 		this.id = id;
 	}
-
+/**
+ * Accessor method to get the Name
+ * @return - The name of the User
+ */
 	public String getName() {
 		return name;
 	}
-
+/**
+ * Accessor method to set the Name
+ * @param name - The name to assign to the user
+ */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+/**
+ * Accessor method to get the email
+ * @return - The email of the user
+ */
 	public String getEmail() {
 		return email;
 	}
-
+/**
+ * Accessor method to set the Email
+ * @param email - The email to assign to the user
+ */
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+/**
+ * Accessor mthod to get the department
+ * @return - The department of the user
+ */
 	public String getDept() {
 		return dept;
 	}
-
+/**
+ * Accessor method to set the department
+ * @param dept - The department to assign to the user
+ */
 	public void setDept(String dept) {
 		this.dept = dept;
 	}
-
+/**
+ * Creates a new instance of a user using the passed in parameter.
+ * The parameter is stripped of Special Escape Character {@link Savable#ESCAPE_CHAR} if present.
+ * @param str - A string containing the properties of the user in csv format.
+ */
 	public User(String str) {
 		String[] arrays = str.split(",");
 		this.id = arrays[0].replace(ESCAPE_CHAR, ',');
@@ -49,7 +108,13 @@ public class User implements Savable, Serializable{
 		this.email = arrays[2].replace(ESCAPE_CHAR, ',');
 		this.dept = arrays[3].replace(ESCAPE_CHAR, ',');
 	}
-	
+/**
+ * Creates a new instance of a user with the properties passed in as parameters.
+ * @param id - The id of the user 
+ * @param name - The name of the user
+ * @param email - The email of the user
+ * @param dept - The department of the user
+ */
 	public User(String id, String name, String email, String dept) {
 		super();
 		this.id = id;
@@ -59,13 +124,15 @@ public class User implements Savable, Serializable{
 	}
 
 	@Override
+	/**
+	 * @return - CSV representation of a user.
+	 */
 	public String asString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(id.replace(',', ESCAPE_CHAR)).append(",")
-		.append(name.replace(',', ESCAPE_CHAR)).append(",")
-		.append(email.replace(',', ESCAPE_CHAR)).append(",")
-		.append(dept.replace(',', ESCAPE_CHAR)).append("\n");
+		sb.append(id.replace(',', ESCAPE_CHAR)).append(",").append(name.replace(',', ESCAPE_CHAR)).append(",")
+				.append(email.replace(',', ESCAPE_CHAR)).append(",").append(dept.replace(',', ESCAPE_CHAR))
+				.append("\n");
 		return sb.toString();
 	}
-	
+
 }
