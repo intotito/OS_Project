2f3d36787ce20576526ad38bc572356394bbea9f
intotito <intotito@gmail.com>
2023-01-02 09:06:27 +0000

    More comments...

--- ie/atu/sw/os/reqres/Formatter.java
+++ ie/atu/sw/os/reqres/Formatter.java
@@ -5,9 +5,28 @@ import java.util.stream.IntStream;
 
 import ie.atu.sw.os.data.Report;
 
+/**
+ * This interface has default and static methods for formatting outputs to the
+ * console. The default method gives inheriting class the capability to print
+ * formatted menus to the screen. And the static method offer generic methods
+ * for formatting tabulated values to the console.
+ * 
+ * @author intot
+ *
+ */
 public interface Formatter {
-	
-	public static void printTabularFeed(String[] entry, int[] ratios, int indent, char corners, char vEdge, char hEdge) {
+	/**
+	 * Prints values at the bottom of a table by replacing the current line.
+	 * 
+	 * @param entry   - Array of values to be output on the console
+	 * @param ratios  - The width of various columns of the table
+	 * @param indent  - Indent to start displaying the feed
+	 * @param corners - Corners characters for the table
+	 * @param vEdge   - Vertical edge characters for the table
+	 * @param hEdge   - Horizontal edge characters for the table
+	 */
+	public static void printTabularFeed(String[] entry, int[] ratios, int indent, char corners, char vEdge,
+			char hEdge) {
 		final int rows = 2;
 		final int cols = entry.length;
 		final int unitWidth = 2;
@@ -17,7 +36,7 @@ public interface Formatter {
 		for (int i = 0; i < rows; i++) {
 			IntStream.range(0, indent).forEach(index -> System.out.print('\t'));
 			System.out.printf("%c", i % 2 == 1 ? corners : vEdge);
-			
+
 			for (int j = 0; j < cols; j++) {
 				if (i % 2 == 1) {
 					IntStream.range(0, WIDTHS[j]).forEach(index -> System.out.printf("%c", hEdge));
@@ -34,6 +53,19 @@ public interface Formatter {
 
 	}
 
+	/**
+	 * This method prints supplied values in tabular form
+	 * 
+	 * @param title          - An array containing titles for the columns
+	 * @param numberOfValues - Number of rows in the table
+	 * @param supplier       - A Function functional interface that gets Each row of
+	 *                       the table given an index
+	 * @param ratios         - The width of various columns of the table
+	 * @param indent         - Indent to start displaying the feed
+	 * @param corners        - Corners characters for the table
+	 * @param vEdge          - Vertical edge characters for the table
+	 * @param hEdge          - Horizontal edge characters for the table
+	 */
 	public static void printTabular(String[] title, int numberOfValues, Function<Integer, String[]> supplier,
 			int[] ratios, int indent, char corners, char vEdge, char hEdge) {
 		final int rows = numberOfValues * 2 + 3;
@@ -63,6 +95,16 @@ public interface Formatter {
 
 	}
 
+	/**
+	 * A variant of the {@link Formatter#getSelectionsAsString(String[]) that
+	 * formats the output with a given indent
+	 * 
+	 * @param options - An array containing the various options for the menu
+	 * @param indent  - The indent to format the menu with
+	 * @return - The formatted menu as a String
+	 * @see Formatter#getSelectionsAsString(String[])
+	 * 
+	 */
 	public default String getStandardOptionsAsString(String[] options, int indent) {
 		StringBuilder sb = new StringBuilder("\n");
 
@@ -79,6 +121,15 @@ public interface Formatter {
 		return sb.toString();
 	}
 
+	/**
+	 * Variant of the {@link Formatter#getSelectionAsString(String[], int)
+	 * getSelectionsAsString} method
+	 * 
+	 * @see Formatter#getSelectionsAsString(String[], int)
+	 * @param options - An array containing different options of the men
+	 * @param indent  - The indentation to format the output with
+	 * @return - The formatted selection prompt
+	 */
 	public default String getStandardSelectionAsString(String[] options, int indent) {
 		StringBuilder sb = new StringBuilder();
 		IntStream.range(0, indent).forEach(j -> sb.append('\t'));
@@ -86,6 +137,15 @@ public interface Formatter {
 		return sb.toString();
 	}
 
+	/**
+	 * Variant of the {@link Formatter#getCancelOptionAsString(String[], int)
+	 * getCancelOptionAsString} method
+	 * 
+	 * @see Formatter#getCancelOptionAsString(String[], int)
+	 * @param options - An array containing different options of the men
+	 * @param indent  - The indentation to format the output with
+	 * @return - The formatted Cancel option
+	 */
 	public default String getStandardCancelOptionAsString(String[] options, int indent) {
 		StringBuffer sb = new StringBuffer();
 
@@ -100,10 +160,26 @@ public interface Formatter {
 		return sb.toString();
 	}
 
+	/**
+	 * This method retrieves the selection prompt as string in the format
+	 * <code>Select options[1-n]></code> where <code>n</code> is the number of
+	 * options.
+	 * 
+	 * @param options - An array containing different options of the menu
+	 * @return - The formatted selection prompt
+	 */
 	public default String getSelectionsAsString(String[] options) {
 		return String.format("\nSelect Options[%d-%d]>", 1, options.length + (hasCancelOption() ? 1 : 0));
 	}
 
+	/**
+	 * An overloaded form of {@link Formatter#getSelectionsAsString(String[])} with
+	 * an optional indentation
+	 * 
+	 * @param options - An array containing different options of the men
+	 * @param indent  - The indentation to format the output with
+	 * @return - The formatted selection prompt
+	 */
 	public default String getSelectionsAsString(String[] options, int indent) {
 		String ind = "";
 		for (int i = 0; i < indent; i++) {
@@ -112,6 +188,12 @@ public interface Formatter {
 		return String.format("\n%sSelect Options[%d-%d]>", ind, 1, options.length + (hasCancelOption() ? 1 : 0));
 	}
 
+	/**
+	 * This method retrieves a formatted menu options as string
+	 * 
+	 * @param options - An array containing the various options for the menu
+	 * @return - The formatted menu as a String
+	 */
 	public default String getOptionsAsString(String[] options) {
 		StringBuilder s = new StringBuilder();
 		for (int i = 0; i < options.length; i++) {
@@ -122,6 +204,14 @@ public interface Formatter {
 		return s.toString();
 	}
 
+	/**
+	 * An overloaded form of {@link Formatter#getOptionsAsString(String[])} with an
+	 * optional indentation
+	 * 
+	 * @param options - An array containing different options of the menu
+	 * @param indent  - The indentation to format the output with
+	 * @return - The formatted menu as a String
+	 */
 	public default String getOptionsAsString(String[] options, int indent) {
 		String ind = "";
 		for (int i = 0; i < indent; i++) {
@@ -136,12 +226,27 @@ public interface Formatter {
 		return s.toString();
 	}
 
+	/**
+	 * Gets the Header
+	 * 
+	 * @return - The header of title of the implementing class
+	 */
 	public abstract String getHeaderAsString();
 
 	public default String getCancelOptionAsString(String[] options) {
 		return hasCancelOption() ? String.format("\n(%d) %s", options.length + 1, getDefaultCancelString()) : "";
 	}
 
+	/**
+	 * An overloaded form of the
+	 * {@link Formatter#getCancelOptionAsString(String[])getCancelOptionAsString}
+	 * method with optional indentation
+	 * 
+	 * @see Formatter#getCancelOptionAsString(String[])
+	 * @param options - An array containing different options of the men
+	 * @param indent  - The indentation to format the output with
+	 * @return - The formatted Cancel option
+	 */
 	public default String getCancelOptionAsString(String[] options, int indent) {
 		String ind = "";
 		for (int i = 0; i < indent; i++) {
@@ -150,14 +255,33 @@ public interface Formatter {
 		return hasCancelOption() ? String.format("\n%s(%d) %s", ind, options.length + 1, getDefaultCancelString()) : "";
 	}
 
+	/**
+	 * A predicate method to determine if the cancel option will be displayed
+	 * 
+	 * @return - True if the cancel option should be displayed or otherwise
+	 */
 	public default boolean hasCancelOption() {
 		return true;
 	}
 
+	/**
+	 * Gets the Cancel option display string
+	 * 
+	 * @return - The Cancel option as String
+	 */
 	public default String getDefaultCancelString() {
 		return "Logout";
 	}
 
+	/**
+	 * This method displays the supplied String in a boxed format
+	 * 
+	 * @param string  - The String to be displayed
+	 * @param indent  - The indentation of the display
+	 * @param corners - Corners characters for the table
+	 * @param vEdge   - Vertical edge characters for the table
+	 * @param hEdge   - Horizontal edge characters for the table
+	 */
 	public static void printBoxed(String string, int indent, char corners, char vEdge, char hEdge) {
 		final int width = string.length() + (((string.length() + 1) % 2) + 3);
 		for (int i = 0; i < 3; i++) {
@@ -174,6 +298,19 @@ public interface Formatter {
 		}
 	}
 
+	/**
+	 * A variant of the {@link Formatter#printBoxed(String, int, char, char, char)
+	 * printBoxed} method with a title text.
+	 * 
+	 * @see Formatter#printBoxed(String, int, char, char, char)
+	 * @param title   - The title to display with the string
+	 * @param string  - The String to be displayed
+	 * @param indent  - The indentation of the display
+	 * @param corners - Corners characters for the table
+	 * @param vEdge   - Vertical edge characters for the table
+	 * @param hEdge   - Horizontal edge characters for the table
+	 * @param skip    - Number of spaces to skip while printing the enclosing box
+	 */
 	public static void printBoxedTitled(String title, String string, int indent, char corners, char vEdge, char hEdge,
 			int skip) {
 		final int width = string.length() + (((string.length() + 1) % 2) + 3);
@@ -197,6 +334,15 @@ public interface Formatter {
 		}
 	}
 
+	/**
+	 * A specialized variant of the
+	 * {@link Formatter#printBoxedTitled(String, String, int, char, char, char, int)
+	 * printBoxedTitled} method that displays an Error message with default title
+	 * and corner and edge characters
+	 * 
+	 * @param msg    - Error message to be displayed
+	 * @param indent - The indentation of the display
+	 */
 	public static void printError(String msg, int indent) {
 		printBoxedTitled("Error", msg, indent, '*', '*', '*', 1);
 	}
